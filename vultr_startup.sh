99eeb8cca6456611f103aa271af46981e16b2780
Shannon Lu <luxiaotong713@sina.com>
2019-08-23 10:47:30 +0800

    Update vultr_startup.sh

--- vultr_startup.sh
+++ vultr_startup.sh
@@ -17,15 +17,17 @@ sudo yum -y install yarn
 ## Run Nuxt
 cd ~/niu_web_local
 yarn install
-yarn build && yarn start &
+#yarn build && yarn start &
+./node_modules/pm2/bin/pm2 start npm -- start
 
 cd ~/niu_web_global
 yarn install
-yarn build && yarn start &
+#yarn build && yarn start &
+../niu_web_local/node_modules/pm2/bin/pm2 start npm -- start
 
 ## Nginx
 sudo yum -y install nginx
 mkdir /etc/nginx/servers
 cp ~/niu_web_local/nginx.conf /etc/nginx/nginx.conf
 cp ~/niu_web_local/niu_web.conf /etc/nginx/servers/niu_web.conf
-sudo nginx &
+sudo nginx
